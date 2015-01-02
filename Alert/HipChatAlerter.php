60eb80faed764c387dbe65f2c35e1903991272c8
Baptiste Langlade <baptouuuu@gmail.com>
2015-01-02 15:04:51 +0100

    #2 add hipchat alerter

---
+++ Alert/HipChatAlerter.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Innmind\ProvisionerBundle\Alert;
+
+use GorkaLaucirica\HipchatAPIv2Client\API\RoomAPI;
+use GorkaLaucirica\HipchatAPIv2Client\Model\Message;
+
+/**
+ * Sends a room notification when an alert is raised
+ */
+class HipChatAlerter implements AlerterInterface
+{
+    protected $api;
+    protected $room;
+
+    /**
+     * Set the hipchat room api
+     *
+     * @param RoomAPI $api
+     */
+    public function setRoomApi(RoomAPI $api)
+    {
+        $this->api = $api;
+    }
+
+    /**
+     * Set the room name where to send notifications to
+     *
+     * @param string $name
+     */
+    public function setRoom($name)
+    {
+        $this->room = (string) $room;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function alert(Alert $alert)
+    {
+        $message = new Message();
+
+        if ($alert->isOverUsed()) {
+            $color = Message::COLOR_RED;
+            $text = sprintf(
+                'Server at full capacity! Command: %s | CPU: %s | Load: %s | Required: %s | Running: %s',
+                (string) $alert->getCommandInput(),
+                $alert->getCpuUsage(),
+                $alert->getLoadAverage(),
+                $alert->getLeftOver(),
+                $alert->getRunningProcesses()
+            );
+        } else {
+            $color = Message::COLOR_YELLOW;
+            $message = sprintf(
+                'Server under used. You may take it down! Command: %s | CPU: %s | Load: %s',
+                (string) $alert->getCommandInput(),
+                $alert->getCpuUsage(),
+                $alert->getLoadAverage()
+            );
+        }
+
+        $message
+            ->setColor($color)
+            ->setNotify(true)
+            ->setMessageFormat(Message::FORMAT_TEXT)
+            ->setMessage($text);
+
+        $this->api->sendRoomNotification($this->room, $message);
+    }
+}
